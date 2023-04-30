a9d5aa02beae14c112248559a2fc83c272101787
lebakhai13 <97871575+lebakhai13@users.noreply.github.com>
2023-04-30 19:04:54 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,6 +55,7 @@ function deleteProduct(id) {
 }
 
 function loadItem(productsArr) {
+    console.log(productsArr);
     var htmls = productsArr.record.map(function(item, index) {
         var title = item.title;
         var brand = item.brand;
